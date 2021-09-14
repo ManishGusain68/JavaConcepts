934c0ebb5c15f22aa7c1e0dfb11406b9df77c4a4
ManishGusain68 <gusainmanish68@gmail.com>
2021-09-14 22:59:43 +0530

    first commit

---
+++ Recursion/src/complesFunction/FactorialDemo.java
@@ -0,0 +1,14 @@
+package complesFunction;
+
+public class FactorialDemo {
+
+	public static void main(String[] args) {
+		ComplexFunction cf=new ComplexFunction();
+		
+		System.out.println("Factorial of 5 is :"+ cf.calculateFactorial(0));
+		
+		
+		
+			
+	}
+}

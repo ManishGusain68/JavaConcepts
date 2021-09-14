53a52d34c50c59ab12852263f44a28e3eb110a4e
ManishGusain68 <gusainmanish68@gmail.com>
2021-09-14 23:01:10 +0530

    first commit

---
+++ StaticNestedClass/src/staticNestedClasses/StaticNestedDemoClass.java
@@ -0,0 +1,10 @@
+package staticNestedClasses;
+
+public class StaticNestedDemoClass {
+	public static void main(String[] args) {
+		
+		OuterClass.StaticNestedClass nestedObjest= new OuterClass.StaticNestedClass();
+		nestedObjest.display();
+	}
+
+}

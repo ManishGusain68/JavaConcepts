1b4336cc24095df24836aa7d597ae552b0b6b42f
ManishGusain68 <gusainmanish68@gmail.com>
2021-09-14 22:56:40 +0530

    first commit

---
+++ loops/src/loops/EnhancedFoorLoop.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class EnhancedFoorLoop {
+public static void main(String[] args)
+{
+	int[] arr= {1,3,2,4,3,4,5,4,3,2};
+	
+	for(int i : arr)
+	{
+		System.out.println("array is ="+ i);
+	}
+}
+}
---
+++ loops/src/loops/ForLoopDemo.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class ForLoopDemo {
+	
+	public static void main(String[] args)
+	{
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Valus of i is ="+ i);
+		}
+	}
+
+}

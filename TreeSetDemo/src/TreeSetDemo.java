d4e9db11934fdfa333cade08b43f4dd37e9972c4
ManishGusain68 <gusainmanish68@gmail.com>
2021-09-14 23:04:01 +0530

    first commit

---
+++ TreeSetDemo/src/TreeSetDemo.java
@@ -0,0 +1,34 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+
+	public  static void main(String[] args)
+	{
+		Set<String> tree=new HashSet<String>();
+		tree.add("das");
+		tree.add("ashish");
+		tree.add("mayuri");
+		tree.add("srishti");
+		tree.add("rocky");
+		tree.add("manish");
+		tree.add("gusain");
+		tree.add("singh");
+		tree.add("sanjeeta");
+		
+		
+		
+		
+		
+		System.out.println(tree);
+		
+//		for (String s : tree)
+//		{
+//			System.out.println("set is : "+ s);
+//		}
+		
+	}
+}
